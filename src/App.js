3a3cd0359a8bf3cf2e3aa8b896d2af74b2e6ef80
talita <talitatravassos@outlook.com>
2020-04-19 18:51:10 -0300

    added material ui components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { Button, makeStyles, TextField } from '@material-ui/core';
+import { AllInclusive } from '@material-ui/icons';
 import React from 'react';
 import './App.scss';
 import Box from './components/Box';
 import { LastFMContext } from './context/LastFMContext';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+
+  cssLabel: {
+    '&:not(hover):not($disabled):not($cssFocused):not($error)': {
+      color: '#d92323',
+    },
+    '&$cssFocused': {
+      color: '#d92323',
+    },
+  },
+  cssOutlinedInput: {
+    '&:not(hover):not($disabled):not($cssFocused):not($error) $notchedOutline': {
+      borderColor: '#d92323', //default
+    },
+    '&:hover:not($disabled):not($cssFocused):not($error) $notchedOutline': {
+      borderColor: '#d92323', //hovered
+    },
+    '&$cssFocused $notchedOutline': {
+      borderColor: '#f71414', //focused
+    },
+  },
+  notchedOutline: {},
+  cssFocused: {
+    color: '#d92323',
+  },
+  error: {},
+  disabled: {},
+}));
+
 function App() {
+  const classes = useStyles();
   const [username, setUsername] = React.useState('');
   const {
     action: { getUsernameData, formatMessage },
@@ -13,7 +59,7 @@ function App() {
   React.useEffect(() => {
     console.log(userData);
     if (userData) {
-      formatMessage(10)
+      formatMessage(10);
     }
   }, [userData, formatMessage]);
 
@@ -26,20 +72,41 @@ function App() {
     <div className='App'>
       <h1>My Last Week FM</h1>
       <div className='container-input'>
-        <form onSubmit={handleSubmit} className='form'>
-          <label htmlFor='username'>Your LastFM Username</label>
-          <input
-            type='text'
+        <form onSubmit={handleSubmit} className={classes.root}>
+          <TextField
+            label='Your LastFM Username'
             placeholder='Your LastFM Username'
             name='username'
             value={username}
+            variant='outlined'
             onChange={(e) => setUsername(e.target.value)}
+            InputLabelProps={{
+              classes: {
+                root: classes.cssLabel,
+                focused: classes.cssFocused,
+              },
+            }}
+            InputProps={{
+              classes: {
+                root: classes.cssOutlinedInput,
+                focused: classes.cssFocused,
+                notchedOutline: classes.notchedOutline,
+              }
+            }}
           />
-          <button type='submit'>GO</button>
+          <Button
+            variant='contained'
+            color='secondary'
+            type='submit'
+            endIcon={<AllInclusive />}
+            size='large'
+          >
+            Go
+					</Button>
         </form>
       </div>
-      <div className="box-container">
-        {(userData && message) ? <Box message={message} /> : <></>}
+      <div className='box-container'>
+        {userData && message ? <Box message={message} /> : <></>}
       </div>
     </div>
   );
